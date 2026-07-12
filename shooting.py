83b340fa335d118aae8878258fc06377fdefe8a7
unknown <hactube13@gmail.com>
2026-07-12 15:55:39 +0900

    1

---
+++ shooting.py
@@ -0,0 +1,65 @@
+import pygame
+
+screen = pygame.display.set_mode([1000, 500])
+
+FPS = 60
+clock = pygame.time.Clock()
+x = 10
+y = 10
+is_right_key_down = False
+left = False
+up = False
+down = False
+space = False
+
+while True:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == 768:
+            if event.key == 1073741906:
+                down = True
+            if event.key == 1073741903:
+                # x+=10
+                is_right_key_down = True
+            if event.key == 1073741904:
+                left = True
+            if event.key == 1073741905:
+                up = True
+
+            if event.key == 32:
+                space = True
+
+            
+        if event.type == 769:
+            if event.key == 1073741906:
+                down = False
+            if event.key == 1073741903:
+                is_right_key_down = False
+            if event.key == 1073741904:
+                left = False
+            if event.key == 1073741905:
+                up = False
+
+            if event.key == 32:
+                space = False
+
+    if is_right_key_down == True:
+        x += 3#.05
+    if up == True:
+        y += 3#.05
+    if down == True:
+        y -= 3#.05
+    if left == True:
+        x -= 3#.05
+
+    screen.fill((0, 0, 0))
+    if space == True:
+        shootx = x
+        shooty = y
+        while (space == True):
+            shooty-=10
+            pygame.draw.rect(screen, (0, 255, 0), (shootx+20,shooty-10,10,30))
+    pygame.draw.rect(screen, (255, 0, 0), (x, y, 50, 50))
+    pygame.display.flip()
+
+    clock.tick(FPS)
